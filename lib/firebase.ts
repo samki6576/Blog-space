5626fb7dd8d279eb97ca8dfea7124d36882f3322
samki6576 <hismm8690@email.com>
2025-08-11 01:30:52 -0700

    add

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAXAtVpeBWpCLJIL2cHAX-xbIFyCY9kF2E",
+  authDomain: "track-3447a.firebaseapp.com",
+  projectId: "track-3447a",
+  storageBucket: "track-3447a.firebasestorage.app",
+  messagingSenderId: "63403525240",
+  appId: "1:63403525240:web:f8736056791bfbe195198d"
 };
 
 // Initialize Firebase only on client side
