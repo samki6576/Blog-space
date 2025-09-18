32d3901afd2dd39306bfdd6724183da65684f85b
Samra <hismm8690@gmail.com>
2025-09-18 04:15:17 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,20 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "BlogSpace - Complete Blog Platform",
   description: "A complete, feature-rich blog platform built with Next.js and Supabase",
-    generator: 'v0.dev'
-}
+  generator: "Samra",
+
+  icons: {
+    icon: [
+      { url: "/r.png", sizes: "32x32" },
+      { url: "/r.png", type: "image/png", sizes: "192x192" },
+      { url: "/r.png", type: "image/png", sizes: "512x512" },
+    ],
+    apple: [
+      { url: "/r.png", type: "image/png", sizes: "180x180" },
+    ],
+  },
+};
+
 
 export default function RootLayout({
   children,
